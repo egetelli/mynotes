a7eac609555e990843db9b87fe81df7ccb56b681
egetelli <egetelli@hotmail.com>
2023-02-28 10:20:26 +0300

    step-14

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/crud/notes_service.dart';
@@ -77,6 +78,7 @@ class _NotesViewState extends State<NotesView> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
+                    case ConnectionState.active:
                       return const Text('Waiting for all notes...');
 
                     default:
